1533aadbe1e2f6bb745d7a27583a71715d5ec8b2
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2025-03-20 16:38:22 +0100

    initialize Flask app and configure SQLAlchemy database

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
 
+from models import db
+
 app = Flask(__name__)
 
+
+def create_app():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all() # Erstellt die Datenbank und Tabellen, wenn sie nicht existieren
+
+    return app
 
 @app.route('/')
 def index():
@@ -9,4 +21,5 @@ def index():
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
